d509b6fc1dc380ae691596bc77ffd80ad6784959
Riza Irawan <riza.dvnl@gmail.com>
2025-08-16 14:54:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -3,10 +3,10 @@
 // Setelan Proyek > Umum > Aplikasi Anda > SDK setup and configuration
 
 export const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId: "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket: "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_YOUR_APP_ID_HERE",
+  apiKey: "AIzaSyCz6scDHqnKk5bW-6QgPV1Pa7N3iYLRGYo",
+  authDomain: "wirucms.firebaseapp.com",
+  projectId: "wirucms",
+  storageBucket: "wirucms.firebasestorage.app",
+  messagingSenderId: "1000004556645",
+  appId: "1:1000004556645:web:3d146c152aaf6ed4f0338c"
 };
